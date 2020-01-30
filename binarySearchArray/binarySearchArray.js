636ff4fd832a9d9c2712d9207fa24ba49be792e6
Emilio-miloCastaneda <57383306+Emilio-miloCastaneda@users.noreply.github.com>
2020-01-30 09:46:38 -0600

    binary search array

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,6 +10,36 @@
  * console.log(index); // null
  */
 
-var binarySearch = function(array, target) {
+//var binarySearch = function(array, target) {
+//    if(array.length >= 0) {
+//        if(target > array.length) {
+//            return null
+//        } else {
+//            for(var i = 0; i < array.length; i++) {
+//                if(i === target) {
+//                    return array[target];
+//                }
+//            }
+//        }
+//    } 
+//};
 
-};
+const binarySearch = function(array, target) {
+    let element;
+    if(array.length > 0) {
+        for(let i =0; i < array.length; i++) {
+            let ele = array[i];
+            element = ele;
+            if(ele === target) {
+                return i;
+            }
+        }
+    }
+    return null;
+}
+
+var bombaclat = [1,2,3,4,5,6,23,56,32,678,31,43,7,7654,3] 
+
+console.log(binarySearch(bombaclat, 678))
+
+
