2d46f3e0cca8a323461af77c1fe22a4d5d91bb12
Emilio-miloCastaneda <57383306+Emilio-miloCastaneda@users.noreply.github.com>
2020-03-19 10:03:47 -0500

    this is the solution i got for balancedbrackets

--- balancedBrackets/balancedBracets.js
+++ balancedBrackets/balancedBracets.js
@@ -10,5 +10,35 @@
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
 const isBalanced = str => {
- 
+    const LB = ['[','(','{']
+    const RB = [']',')','}']
+    const leftCount = 0;
+    const rightCount = 0; 
+
+    for(let i = 0; i < str.length; i++) {
+        let countL;
+        let countR;
+        if(str[i] === LB[0] || LB[1] || LB[2]) {
+            countL++
+        } else if(str[i] === RB[0] || RB[1] || RB[2]) {
+            countR++
+        }
+        if(leftCount == rightCount) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 };
+
+    //if the string contains brackets check to see if the brackets have an even occurence of opening and closing brackets in the string
+        //go to each element in the string and see if it corresponds to the left or right bracket
+        //if they have an even occurence of left and right brackets 
+            //increment the left and right count by one and store the total.
+    //if the left total is not equal to the right total
+        //return false
+    //else return true
+
+    /* I believe i was close to solving it but granted for me it was using what i felt most comfortable with and that is for loops and arrays*/
+
+    console.log(isBalanced('[{]}'))
